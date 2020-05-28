fef5d251c85496b5b65511b49d764fce3ce0bd37
Benoît Hubert <benoithubert@gmail.com>
2020-05-28 15:04:59 +0200

    Setup CORS and send JSON data

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CLIENT_APP_ORIGIN,
+};
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+
+app.get('/api/meme', (req, res) => res.json({
+  title: 'Node.js debugging meme',
+  pictureUrl: 'https://miro.medium.com/max/1000/1*9bDq6pyYoXa39QxldAkf-g.jpeg'
+}));
 
 app.listen(process.env.PORT, (err) => {
   console.log('starting up server', err);
